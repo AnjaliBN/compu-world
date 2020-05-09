60d1b9fa068d87e3fd215bd67500df6eb30221d1
Anjali B N <bnanjali@gmail.com>
2020-05-09 22:25:10 +0530

    Adding version 1.0 files of Compu-World website

---
+++ js/scripts.js
@@ -0,0 +1,229 @@
+const sr = ScrollReveal();
+
+// Home Section
+
+sr.reveal('.homeHeader', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.homeCaption', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.homeLogo', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.homeBtn', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+// Home Section
+
+// About Section
+
+sr.reveal('.aboutHeader', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.aboutCaption', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.aboutBtn', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+// About Section
+
+// Services Section
+
+sr.reveal('.servicesCaption', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.services-item', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+// Services Section
+
+// Why Us Section
+
+sr.reveal('.whyUsHeader', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.whyUsText', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.whyUsItems', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+// Why Us Section
+
+// Process section
+
+sr.reveal('.processBriefing', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.companies', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+// Process section
+
+// Contact section
+
+sr.reveal('.contactHeader', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 200,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.contactText', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 200,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+sr.reveal('.contactContents', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 200,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in',
+    viewFactor: 0.0
+});
+
+// Contact section
