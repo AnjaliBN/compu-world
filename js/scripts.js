908fd904318801fdc106091106253e2dd6ae9b15
Anjali B N <bnanjali@gmail.com>
2020-05-26 16:46:33 +0530

    Adding the completed redesigned wesbite along with the related files

--- js/scripts.js
+++ js/scripts.js
@@ -1,243 +1,701 @@
+const sr = ScrollReveal();
 
+// index.html
 
-// const sr = ScrollReveal();
+sr.reveal('.aboutMainCaption', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Home Section
+sr.reveal('.aboutSubCaption', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.homeHeader', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.wtitle', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.homeCaption', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.shadowDiv', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.homeLogo', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.servicesImageDiv', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.homeBtn', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.services-heading', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Home Section
+sr.reveal('.serviceFeaturesContainer', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // About Section
+sr.reveal('.servicesBtnDiv', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.aboutHeader', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.team', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.aboutCaption', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.whyUsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.aboutBtn', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.whyUsText', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // About Section
+sr.reveal('.whyUsReasons', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Services Section
+sr.reveal('.processBriefing', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.servicesCaption', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.processHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.services-item', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.processText', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Services Section
+sr.reveal('.companies', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Why Us Section
+sr.reveal('.reasons', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.whyUsHeader', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.processImages', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.whyUsText', {
-//     origin: 'bottom',
-//     distance: '50px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.contactTitle', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.whyUsItems', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.contactHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Why Us Section
+sr.reveal('.contactText', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Process section
+sr.reveal('.contactWays', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.processBriefing', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+//For all products & services types & subtypes
 
-// sr.reveal('.companies', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.card', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.processImages', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+//computerProducts.html
 
-// // Process section
+sr.reveal('.computerProductsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Contact section
+//surveillanceAndLogin.html
 
-// sr.reveal('.contactHeader', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.surveillanceLoginSystemsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// sr.reveal('.contactText', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+//networking.html
 
-// sr.reveal('.contactInfo', {
-//     origin: 'bottom',
-//     distance: '30px',
-//     duration: 1000,
-//     reset: true,
-//     mobile: true,
-//     desktop: true,
-//     useDelay: 'always',
-//     easing: 'ease-in-out',
-//     viewFactor: 0.0
-// });
+sr.reveal('.networkingHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
 
-// // Contact section
+//softwareAndDev.html
+
+sr.reveal('.softwareAndDevHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//For all products & services subtypes
+
+sr.reveal('.more-info', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//computerProductBrands.html
+
+sr.reveal('.desktopsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.laptopsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.notebooksHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.handheldsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.computerAccessoriesHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//printerBrands.html
+
+sr.reveal('.printersHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.multiFunctionalPrintersHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.faxHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.copiersHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.printServersHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.largeFormatPrintersHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.plottersHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//upsPowerBackupsBrands.html
+
+sr.reveal('.upsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.powerBackupsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//cctvDvrtypes.html
+
+sr.reveal('.cctvDvrHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.cctvDvrTypesHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//cctvBrands.html
+
+sr.reveal('.cctvBrandsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//biometricLoginSystems.html
+
+sr.reveal('.biometricHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//cardSwipeSystems.html
+
+sr.reveal('.cardSwipeSystemsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//barcodeScanners.html
+
+sr.reveal('.barcodeScannersHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//fingerprintScanners.html
+
+sr.reveal('.fingerPrintScannerHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//networkingDevices.html
+
+sr.reveal('.networkingDevicesHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//networkSoftware.html
+
+sr.reveal('.networkSoftwareHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//smartDigitalEducation.html
+
+sr.reveal('.smartDigitalEducationHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+//softwareApplications.html
+
+sr.reveal('.osHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.microsoftOfficeApplicationsHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.antivirusSoftwareHeader', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 700,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
